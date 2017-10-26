462075e2af664569a99ea6e3c91c9a3326fc4e54
nainanikapil1 <nainanikapil1@gmail.com>
2017-10-26 10:42:10 +0530

    cmc api integratino

---
+++ server/src/restCall.js
@@ -0,0 +1,53 @@
+const unirest = require('unirest');
+let accessToken = null;
+
+module.exports={
+    getAccessToken: function () {
+        return new Promise(function (resolve, reject) {
+        unirest.post('https://cmc-api.appirio.net/v_beta/oauth/token')
+        .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+        .send({ 'refreshToken':'5Aep861eWO5D.7wJBu2NtzW7PW9j3lEhak9yQSAz.PvEw6_70xaIWM.menjrwEJrro2OQSluKRJbdLiYARRqVI7' })
+        .end(function (response) {
+            if(response.statusCode ==200){
+                accessToken = response.body.accessToken;
+                console.log(accessToken);
+                 resolve(accessToken);   
+            }
+            else{
+                reject(response)
+            }
+            
+        });
+    })
+    },
+
+    storyCheck:function (AccessToken,StoryNumber) {
+        return new Promise(function (resolve, reject) {
+            console.log(encodeURI("query=storyNumber like '%"+StoryNumber + "%'"));
+            let Url = "https://cmc-api.appirio.net/v_beta/stories?"+ encodeURI("query=storyNumber like '%"+ StoryNumber + "%'");
+            unirest.get(Url)
+            .headers({'Accept': 'application/json', 'AuthorizationToken': AccessToken})
+            .end(function (response) {
+                resolve(JSON.stringify(response.body.content.records[0].description));
+            });
+        })
+    },
+
+    fetchProductStories: function (ProductName,AccessToken) {
+        return new Promise(function (resolve, reject) {
+            console.log(encodeURI("query=Product__c like '%")+ProductName + "%'");
+            let Url = "https://cmc-api.appirio.net/v_beta/stories?query=Product__c%20like%20'%25"+ProductName+"%25'";
+            console.log(Url);
+            unirest.get(Url)
+            .headers({'Accept': 'application/json', 'AuthorizationToken': AccessToken})
+            .end(function (response) {
+                if(response.statusCode==200){
+                    resolve(JSON.stringify(response.body.content.records));
+                }
+             else{
+                 reject(response);
+             }
+            });
+        })
+    }
+};
